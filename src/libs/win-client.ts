71bce739bbe795a04c68adee656d7ade6b647a0a
oHTGo <nguyennhathuy.orit@gmail.com>
2024-02-04 12:45:20 +0700

    Update code

--- src/libs/win-client.ts
+++ src/libs/win-client.ts
@@ -25,7 +25,7 @@ class WinClient implements WARPClient {
   }
 
   async install() {
-    await exec.exec('choco.exe install -y warp');
+    await exec.exec('choco install -y warp');
     core.addPath('C:\\Program Files\\Cloudflare\\Cloudflare WARP\\');
   }
 
